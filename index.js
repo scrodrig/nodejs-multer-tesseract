616c9b56736a88f8b13bf8f7ca83d132017549a0
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-18 15:16:51 +0100

    refactor: Comment out Tesseract import and OCR processing code
    This commit comments out the import statement for the Tesseract.js library and the code related to OCR processing. The OCR functionality was causing issues and needs to be temporarily disabled. This change ensures that the application runs without errors until the OCR processing is fixed.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const Tesseract = require("tesseract.js");
+// const Tesseract = require("tesseract.js");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -14,22 +14,42 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.post("/upload", upload.single("uploadedImage"), async (req, res) => {
-  console.log(req.file);
+app.use('/uploadImage', upload.single("uploadedImage"),require('./routes/imagesRoutes'));
 
-  try {
-    const worker = await Tesseract.createWorker("eng");
-    const ret = await worker.recognize("uploads/" + req.file.filename);
-    console.log(ret.data.text);
-    await worker.terminate();
+// app.post("/upload/en", upload.single("uploadedImage"), async (req, res) => {
+//   console.log(req.file);
 
-    return res.json({ text: ret.data.text });
-  } catch (err) {
-    console.log(err);
-  }
-});
+//   try {
+//     const worker = await Tesseract.createWorker("eng");
+//     const ret = await worker.recognize("uploads/" + req.file.filename);
+//     console.log(ret.data.text);
+//     await worker.terminate();
+
+//     return res.json({ text: ret.data.text });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+
+
+// app.post("/upload/pt", upload.single("uploadedImage"), async (req, res) => {
+//   console.log(req.file);
+
+//   try {
+//     const worker = await Tesseract.createWorker("por");
+//     const ret = await worker.recognize("uploads/" + req.file.filename);
+//     console.log(ret.data.text);
+//     await worker.terminate();
+
+//     return res.json({ text: ret.data.text });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
